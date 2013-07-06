f3bc54c16b2a3c56921d3ed75a587daa493b8aeb
Ho Hiep <hiephm@trikien.com>
2013-07-06 21:14:21 +0000

    Original

---
+++ config/initializers/rails_6440_patch.rb
@@ -0,0 +1,9 @@
+# file config/initializers/rails_6440_patch.rb
+ActionController::Request.class_eval do
+    def reset_session
+      # session may be a hash, if so, we do not want to call destroy
+      # fixes issue 6440
+      session.destroy if session and session.respond_to?(:destroy)
+      self.session = {}
+    end
+end
